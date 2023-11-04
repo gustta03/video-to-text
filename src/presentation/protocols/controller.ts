a5feb67c14c0a72f3ab9eb67d23682603db3ef5a
Gustavo Alves <gustavoalves03.dev@gmail.com>
2023-11-04 19:26:07 -0300

    add videos-history protocol

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -3,7 +3,3 @@ import { HttpBodyResponse } from './http'
 export interface Controller<T = any> {
   handle: (request: T) => Promise<HttpBodyResponse>
 }
-
-export interface ControllerGPT<T = any> {
-  handle: (request: T, res: Request) => Promise<HttpBodyResponse>
-}
